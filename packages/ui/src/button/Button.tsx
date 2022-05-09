9da8d026cf93d9d3bc233b2541f672d19693778c
vino <vino0908@outlook.com>
2022-05-09 17:40:13 +0800

    feat: 完善Button组件

--- packages/ui/src/button/Button.tsx
+++ packages/ui/src/button/Button.tsx
@@ -1,8 +1,31 @@
-import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+import { computed, defineComponent } from 'vue'
+import './style'
 
 export default defineComponent({
   name: 'DButton',
-  setup() {
-    return () => <button>测试</button>
+  props: {
+    type: {
+      type: String as PropType<'primary' | 'default' | 'danger'>,
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String as PropType<'small' | 'middle' | 'large'>,
+      default: 'middle',
+    },
+  },
+  setup(props, { slots }) {
+    const classes = computed(() => {
+      return ['dtd-button', `dtd-button-${props.type}`]
+    })
+    return () => (
+      <button class={classes.value} disabled={props.disabled}>
+        <span>{slots.default?.()}</span>
+      </button>
+    )
   },
 })
