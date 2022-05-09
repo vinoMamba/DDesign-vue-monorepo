907e36c5ae12b17e0ee4ffdb1191ee085d1fbd52
vino <vino0908@outlook.com>
2022-05-09 23:16:33 +0800

    chore: 添加对UI库的监听

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      ignored: ['!**/node_modules/@vino/**'],
+    },
+  },
 })
