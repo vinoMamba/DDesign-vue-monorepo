d35d9dfaa7a989aa3296c777d820f1324c703881
vino <vino0908@outlook.com>
2022-05-09 16:40:28 +0800

    fix: 消除 TS 报错

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,6 +6,7 @@ export * from './components'
 export default {
   install(app: App) {
     for (const componentKey in components) {
+      // @ts-expect-error @ts-expect-error
       const component = components[componentKey]
       if (component.install) {
         component.install(app)
