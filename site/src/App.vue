b40c8cec2dfc52944577dbc4fea20e7270dadd77
vino <vino0908@outlook.com>
2022-05-09 17:40:42 +0800

    feat: 添加按钮组件demo

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import { DButton } from '@vino/dt-design'
+  import '@vino/dt-design/es/style.css'
 </script>
 
 <template>
   <h1>Site</h1>
-  <DButton />
+  <DButton type="primary">确定</DButton>
 </template>
