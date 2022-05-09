cc9d44ed08251f07876f9a7b102234a71b543ec6
vino <vino0908@outlook.com>
2022-05-09 13:14:25 +0800

    chore: 调整eslint 配置

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,9 +6,10 @@ export * from './components'
 export default {
   install(app: App) {
     for (const componentKey in components) {
-      // eslint-disable-next-line import/namespace
       const component = components[componentKey]
-      if (component.install) component.install(app)
+      if (component.install) {
+        component.install(app)
+      }
     }
     return app
   },
