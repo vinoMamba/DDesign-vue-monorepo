4a91475964a10e12a68e918e1c265eb3077340b8
vino <vino0908@outlook.com>
2022-05-09 16:33:48 +0800

    feat: 打包成功

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), dts()],
+  build: {
+    target: 'modules',
+    outDir: 'es',
+    minify: false,
+    rollupOptions: {
+      external: ['vue'],
+      input: ['src/index.ts'],
+      output: [
+        {
+          format: 'es',
+          dir: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+        {
+          format: 'cjs',
+          dir: 'lib',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+      ],
+    },
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['cjs', 'es'],
+    },
+  },
+})
