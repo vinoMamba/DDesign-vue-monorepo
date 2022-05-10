51c3159e4d1eb8a865c17f94ae6f6a069fe6761d
vino <vino0908@outlook.com>
2022-05-10 15:56:21 +0800

    feat: popover 组件支持trigger属性

--- packages/ui/src/popover/Popover.tsx
+++ packages/ui/src/popover/Popover.tsx
@@ -1,15 +1,72 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, nextTick, onMounted, onUnmounted, ref, type PropType } from 'vue'
 import './style'
 
 export default defineComponent({
   name: 'DPopover',
+  props: {
+    trigger: {
+      type: String as PropType<'click' | 'hover'>,
+      default: 'click',
+    },
+  },
   setup(props, { slots }) {
+    const popoverRef = ref<HTMLDivElement | null>(null)
     const visible = ref(false)
-    const toggleVisibility = () => {
-      visible.value = !visible.value
+
+    function onClickOutside(event: MouseEvent) {
+      if (popoverRef.value && !popoverRef.value.contains(event.target as Node)) {
+        close()
+      }
     }
+    function open() {
+      visible.value = true
+      nextTick(() => {
+        document.addEventListener('click', onClickOutside)
+      })
+    }
+
+    function close() {
+      visible.value = false
+      nextTick(() => {
+        document.removeEventListener('click', onClickOutside)
+      })
+    }
+
+    function toggleVisibility(event: MouseEvent) {
+      if (popoverRef.value && popoverRef.value.contains(event.target as Node)) {
+        visible.value ? close() : open()
+      }
+    }
+
+    function bindEventToPopover() {
+      if (popoverRef.value) {
+        if (props.trigger === 'click') {
+          popoverRef.value.addEventListener('click', toggleVisibility)
+        } else {
+          popoverRef.value.addEventListener('mouseenter', open)
+          popoverRef.value.addEventListener('mouseleave', close)
+        }
+      }
+    }
+    function unbindEventToPopover() {
+      if (popoverRef.value) {
+        if (props.trigger === 'click') {
+          popoverRef.value.removeEventListener('click', toggleVisibility)
+        } else {
+          popoverRef.value.removeEventListener('mouseenter', open)
+          popoverRef.value.removeEventListener('mouseleave', close)
+        }
+      }
+    }
+
+    onMounted(() => {
+      bindEventToPopover()
+    })
+    onUnmounted(() => {
+      unbindEventToPopover()
+    })
     return () => (
-      <div class="dtd-popover" onClick={() => toggleVisibility()}>
+      <div class="dtd-popover" ref={popoverRef}>
         {visible.value ? <div class="dtd-popover-content-wrapper">{slots.content?.()}</div> : null}
         {slots.default?.()}
       </div>
--- packages/ui/src/popover/__tests__/popover.test.ts
+++ packages/ui/src/popover/__tests__/popover.test.ts
@@ -28,5 +28,35 @@ describe('DPopover', () => {
     })
     await wrapper.trigger('click')
     expect(wrapper.html()).toContain('content')
+    await wrapper.trigger('click')
+    expect(wrapper.html()).not.toContain('content')
+  })
+  it('cant set trigger prop', async () => {
+    const wrapper = mount(DPopover, {
+      slots: {
+        content: () => '<p>content</p>',
+        default: () => '<button>按钮</button>',
+      },
+      props: {
+        trigger: 'hover',
+      },
+    })
+    await wrapper.trigger('mouseenter')
+    expect(wrapper.html()).toContain('content')
+    await wrapper.trigger('mouseleave')
+    expect(wrapper.html()).not.toContain('content')
+  })
+  it('can close the popover when click outside', async () => {
+    const wrapper = mount(DPopover, {
+      slots: {
+        content: () => '<p>content</p>',
+        default: () => '<button>按钮</button>',
+      },
+    })
+    await wrapper.trigger('click')
+    expect(wrapper.html()).toContain('content')
+    document.body.addEventListener('click', () => {
+      expect(wrapper.html()).not.toContain('content')
+    })
   })
 })
