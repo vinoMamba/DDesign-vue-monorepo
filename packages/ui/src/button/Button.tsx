737a6cb2a3f89f390e104bcf03ebccbf0e89f85a
vino <vino0908@outlook.com>
2022-05-10 09:59:03 +0800

    feat: 完成按钮组件开发

--- packages/ui/src/button/Button.tsx
+++ packages/ui/src/button/Button.tsx
@@ -17,13 +17,23 @@ export default defineComponent({
       type: String as PropType<'small' | 'middle' | 'large'>,
       default: 'middle',
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props, { slots }) {
     const classes = computed(() => {
-      return ['dtd-button', `dtd-button-${props.type}`]
+      return [
+        'dtd-button',
+        `dtd-button-${props.type}`,
+        `dtd-button-${props.size}`,
+        `${props.loading ? 'dtd-button-loading' : ''}`,
+      ]
     })
     return () => (
-      <button class={classes.value} disabled={props.disabled}>
+      <button class={classes.value} disabled={props.disabled || props.loading}>
+        {props.loading ? <i class="dtd-button-loading-icon"></i> : null}
         <span>{slots.default?.()}</span>
       </button>
     )
