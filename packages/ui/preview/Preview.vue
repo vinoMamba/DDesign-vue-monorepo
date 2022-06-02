23a68aa072f9970f6eab395f42da0553c3ac1bec
vino <vino0908@outlook.com>
2022-06-02 10:40:13 +0800

    fix: 限制弹出框最大高度

--- packages/ui/preview/Preview.vue
+++ packages/ui/preview/Preview.vue
@@ -1,7 +1,6 @@
 <template>
   <main>
-    {{ checked }}
-    <DInput @focus="toggle" />
+    <DInput @focus="toggle" v-model:value="inputValue" />
     <DUserTree
       v-model:visible="visibleRef"
       :tree-data="treeData"
@@ -14,11 +13,12 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, watch } from 'vue'
   import { DUserTree, DInput } from '../src'
   import { mockTree } from '../src/userTree/mock'
   import { TreeNode } from '../src/userTree/type'
   const checked = ref()
+  const inputValue = ref('')
   const visibleRef = ref(false)
   function toggle() {
     visibleRef.value = !visibleRef.value
@@ -26,6 +26,13 @@
   const treeData = ref<TreeNode[]>([])
   onMounted(() => {
     treeData.value = mockTree
+  })
+  watch(checked, (val) => {
+    if (val.length > 0) {
+      inputValue.value = val[0].name
+    } else {
+      inputValue.value = ''
+    }
   })
 </script>
 <style scoped>
